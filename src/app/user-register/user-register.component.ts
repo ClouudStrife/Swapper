7da2f6a0d0dfafdd91030e93eca7a34f079cbc8a
CAIO <caio.b.santos@hotmail.com>
2018-11-15 15:54:47 -0200

    Ajustes para criação dos requests

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css']
 })
+
+
+
+
 export class UserRegisterComponent{
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  email: string;
+  senha: string;
 
   cadastrarUsuario(form){
+    this.email = form.value.Email;
+    this.senha = form.value.Senha;
+
+    this.http.post('http://localhost:3000/api/add', { "email": this.email, "senha": this.senha})
+      .subscribe(dados => console.log(dados));
+    
+    //form.reset();
     console.log(form);
   }
   
