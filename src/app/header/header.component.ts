09fa0d934c0a09a9aea572b3a402c5ab7caa5a2b
Guilherme Franco <guiffranco1@gmail.com>
2018-12-03 11:21:15 -0200

    Modifica o header caso usuário esteja logado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  conta = "Entrar"
+  rota = "userLogin"
+  usuario = "";
+  usuarioJSON = "";
+  show = false;
 
   ngOnInit() {
-    if(localStorage.getItem('userToken'){
+    if(localStorage.getItem('userToken')){
       this.conta = "Minha conta";
       this.rota = "userMain"
+      this.usuarioJSON = localStorage.getItem("user");
+      this.usuario = JSON.parse(this.usuarioJSON)
+      this.show = true;
     }
     else{
       this.conta = "Entrar"
